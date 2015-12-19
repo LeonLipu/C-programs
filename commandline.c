ee6f03fd1cc35fcc3eff60b2c85a6ab435c2a875
leonlipu <lipusipupara@gmail.com>
2015-12-20 03:15:23 +0530

    datastructor stack got implented

--- commandline.c
+++ commandline.c
@@ -1,8 +1,6 @@
 #include "stdio.h"
 void main (int args, char *argv[])
 {
-
-
         int i =0;
         for (i = 0; i  <args; i++) {
                 printf("%s\n",argv[i] );
--- datastructure/stack.c
+++ datastructure/stack.c
@@ -1,27 +1,108 @@
 #include <stdio.h>
+
 #define max 5
-typedef struct stack{
-
-  int stk[max];
-  int top;
-
+typedef struct stack {
+        int stk[max];
+        int top;
 } SK;
 
-void push(void );
-int  pop(void);
-void display(void);
+void  push();
+void  pop();
+void display();
 
-
-int main(){
-
-
-
-
-  printf("%d\n", max);
+SK sk;
 
 
 
+void  main()
+{
+        sk.top=-1;
+        int flag=1;
+        int option;
+        printf("%d\n",sk.top );
 
 
-  return 0;
+        while (flag) {
+
+
+                printf(" Enter your option ");
+                printf(" 1. push  ");
+                printf(" 2. pop  ");
+                printf(" 2. display  ");
+
+                scanf("%d\n", &option);
+
+                switch (option) {
+                case 1:
+                        push();
+                        break;
+                case 2:
+                        pop();
+                        break;
+
+                case 3:
+                        display();
+                        break;
+                }
+                fflush (stdin);
+                printf("are u sure want to continue");
+                scanf("%d\n",&flag ); //0 for exit and 1 for continue
+            
+        }
+}
+
+
+void push (){
+printf("%d\n", sk.top);
+  if(sk.top>=max-1)
+  {
+    printf("stack reached max ");
+    return ;
   }
+  else {
+    int no;
+    printf("enter the no you want to push ");
+  scanf("%d\n",&no );
+  sk.top++;
+  sk.stk[sk.top]=no;
+  return ;
+  }
+
+}//end of push function
+
+void pop (){
+
+  if(sk.top<0)
+  {
+    printf("stack has no element  ");
+    return ;
+  }
+  else {
+    int no=sk.stk[sk.top];
+  printf(" it poped element %d\n", no);
+  sk.top--;
+  return ;
+
+  }
+
+}
+
+
+void display (){
+
+  if(sk.top<0)
+  {
+    printf("stack has no element  ");
+    return ;
+  }
+  else {
+
+    int i;
+                  for(i=0;i<=sk.top;i++)
+                {
+                        printf("%d\n", sk.stk[i]);
+                }
+                return ;
+  }
+
+}
